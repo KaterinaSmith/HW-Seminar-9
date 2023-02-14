e7b89d4ce0057491e239fabf5b508719af785890
Katerina <katerina3113@bk.ru>
2023-02-14 13:11:30 +0300

    Домашнее задание 1

---
+++ hm1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+TopToLow(num, count);
+
+
+void TopToLow(int n, int count)
+{
+    if (count > n)
+    {
+        return;
+    }
+    else
+    {
+        TopToLow(n, count + 1);
+        Console.Write(count + ", ");
+    }
+}
